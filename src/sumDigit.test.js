4a4c76cdb2dcf6e3b9ae0446033c4e9fa47d96fb
denis <denis.baklykov@boxberry.ru>
2022-08-12 22:18:36 +0300

    поправила тесты для функции c суммой цифр

--- src/sumDigit.test.js
+++ src/sumDigit.test.js
@@ -1,22 +1,14 @@
 import {sumDigit} from "./sumDigit";
 
-let originalPrompt;
-
 describe("sumDigit",()=>{
     beforeEach(()=>{
        jest.clearAllMocks();
-       originalPrompt=prompt;
-    });
-    afterEach(()=>{
-       prompt=originalPrompt;
     });
     [{a:211, result: 4},{a:803, result: 11},{a:732, result: 12}].forEach(({a,result})=>{
         it("сумма цифр " + a + " равна " + result,
             () => {
-                prompt = (x) => {
-                   return a;
-                }
                 jest.spyOn(console, "log");
+                jest.spyOn(window, "prompt").mockImplementation(() => a);
                 sumDigit();
                 expect(console.log).toHaveBeenCalledWith(result);
             }) ;
