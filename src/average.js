07183b8bc4f507521a2345306cbb6a1136fdbed1
denis <denis.baklykov@boxberry.ru>
2022-08-16 17:54:23 +0300

    среднеарифметическое нечетных чисел

---
+++ src/average.js
@@ -0,0 +1,15 @@
+/*Запросить у пользователя ввод числа N. Вывести в
+консоль среднее арифметическое всех нечётных
+чисел от 1 до N.*/
+export function average(){
+    const N=prompt("введите N");
+    let num=0;
+    let sum=0;
+    for (let i=1;i<=N;i++){
+        if(i%2===1){
+            sum+=i;
+            num++;
+        }
+    }
+    console.log(sum/num);
+}
