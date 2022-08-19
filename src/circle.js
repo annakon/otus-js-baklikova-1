2b811fb7255ee5c7f5fec0e71ae5804a21abf394
denis <denis.baklykov@boxberry.ru>
2022-08-19 08:54:10 +0300

    длина окружности, площадь круга

---
+++ src/circle.js
@@ -0,0 +1,5 @@
+export function circle(r){
+    let C=2*Math.PI*r;
+    let S=Math.PI*r*r;
+    console.log("Длина окружности " + C.toFixed(2)+" площадь круга " + S.toFixed(2));
+}
---
+++ src/circle.test.js
@@ -0,0 +1,18 @@
+import {circle} from "./circle"
+
+describe("circle",()=>{
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+    [
+        { r:1, S: 3.14, C: 6.28},
+        { r:2, S: 12.57, C: 12.57},
+        { r:5, S: 78.54, C: 31.42}
+    ].forEach(({ r,S,C }) =>
+        it(`prints ${S} and ${C} for ${r}`, () => {
+            jest.spyOn(console, "log");
+            circle(r);
+            expect(console.log).toHaveBeenCalledWith("Длина окружности " + C+" площадь круга " + S);
+        })
+    );
+});
